38e879b5f5fc09a9031a1b542b7697d73ec8b8d0
Nahuel <nahuel.lunacab@gmail.com>
2026-05-26 08:25:49 -0300

    Configuracion de variables de entorno

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:8082/api"
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8082/api',
 });
 
-export default api;
+export default apiClient;
